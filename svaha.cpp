088b682b1c886e5da4e0e890d311e3c86d823fa9
bathory <65055208+xdexix@users.noreply.github.com>
2022-07-20 20:57:29 +0300

    Предпоследняя версия

--- svaha.cpp
+++ svaha.cpp
@@ -1,37 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <windows.h>
 #include <fstream>
 #include <iomanip>
 #include <sstream>
+#include <conio.h>
 
 using namespace std;
 
 #include "format.h"
 #include "user.h"
+#include "process.h"
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    system("mode con cols=120 lines=50");
+    system("mode con cols=122 lines=50");
 
-    vector<user> users; int i = 1;
-    while (users.size() < users.max_size())
-    {
-        if (file_exist(i))
-        {
-            user temp;
-            users.push_back(temp.read_user(i));
-            i++;
-        }
-        else break;
-    }
-    for (user n : users)
-    {
-        n.print_user_info();
-        n.print_user_dreams();
-        cout << endl;
-    }
+    menu();
 }
