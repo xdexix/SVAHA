215e2f752edc7a868d015415c7683b6d30e84d77
bathory <65055208+xdexix@users.noreply.github.com>
2022-07-22 20:44:54 +0300

    Инклуды для проекта

---
+++ includes.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <conio.h>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+#include "format.h"
+#include "user.h"
+#include "process.h"
+
+#endif
